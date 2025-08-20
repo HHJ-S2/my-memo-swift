c39ac37d866310b7676edf3484365fd8cf69a051
HHJ-S2 <berdle0122@gmail.com>
2025-08-20 11:16:36 +0900

    TableView cell - dataSource 추가

--- Memo/Model/DataManager.swift
+++ Memo/Model/DataManager.swift
@@ -18,29 +18,48 @@ class DataManager {
     return persistentContainer.viewContext
   }
   
+  // 메모 목록
+  var list = [MemoEntity]()
+  
   // MARK: - Core Data stack
-
+  
   lazy var persistentContainer: NSPersistentContainer = {
-      let container = NSPersistentContainer(name: "Memo")
-      container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-          if let error = error as NSError? {
-              fatalError("Unresolved error \(error), \(error.userInfo)")
-          }
-      })
-      return container
-  }()
-
-  // MARK: - Core Data Saving support
-
-  func saveContext () {
-      let context = persistentContainer.viewContext
-      if context.hasChanges {
-          do {
-              try context.save()
-          } catch {
-              let nserror = error as NSError
-              fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-          }
+    let container = NSPersistentContainer(name: "Memo")
+    container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+      if let error = error as NSError? {
+        fatalError("Unresolved error \(error), \(error.userInfo)")
       }
+    })
+    return container
+  }()
+  
+  // MARK: - Core Data Saving support
+  
+  func saveContext () {
+    let context = persistentContainer.viewContext
+    if context.hasChanges {
+      do {
+        try context.save()
+      } catch {
+        let nserror = error as NSError
+        fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+      }
+    }
+  }
+  
+  // 메모 목록 패칭
+  func fetch() {
+    let request = MemoEntity.fetchRequest()
+    
+    // 내림차순 정렬
+    let sortByDateDesc = NSSortDescriptor(key: "insertDate", ascending: false)
+    
+    request.sortDescriptors = [sortByDateDesc]
+    
+    do {
+      list = try mainContext.fetch(request)
+    } catch {
+      print(error)
+    }
   }
 }
