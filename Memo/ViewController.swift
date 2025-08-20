99c6471c4ab866263af52161fec9c002fdf18132
HHJ-S2 <berdle0122@gmail.com>
2025-08-20 09:23:28 +0900

    Initial Commit

---
+++ Memo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Memo
+//
+//  Created by any on 8/20/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
