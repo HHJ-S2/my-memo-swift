6e21e6044d28fe359b3ceba3a9cfb16adc486110
HHJ-S2 <berdle0122@gmail.com>
2025-08-20 13:45:16 +0900

    더미데이터 batch insert

--- Memo/Model/DataManager.swift
+++ Memo/Model/DataManager.swift
@@ -47,6 +47,60 @@ class DataManager {
     }
   }
   
+  func insertDummyData() {
+    #if DEBUG
+    let countRequest = MemoEntity.fetchRequest()
+    
+    do {
+      let count = try mainContext.count(for: countRequest)
+      
+      if count > 0 { return }
+    } catch {
+      print(error)
+    }
+    
+    guard let path = Bundle.main.path(forResource: "lipsum", ofType: "txt") else { return } // 파일 경로
+    
+    do {
+      let source = try String(contentsOfFile: path) // 더미데이터 문자열
+      
+      let sentences = source.components(separatedBy: .newlines).filter { str in
+        str.trimmingCharacters(in: .whitespacesAndNewlines).count > 0 // 공백, 줄바꿈 제거
+      }
+      
+      var dataList = [[String: Any]]()
+      
+      for sentence in sentences {
+        /**
+        let memo = MemoEntity(context: mainContext) // Entity 인스턴스 생성
+        memo.content = sentence
+        
+        // 랜덤 날짜 생성 - 현재날짜 기준 +=30
+        let randomDate = Date(timeIntervalSinceNow: Double.random(in: 0 ... 3600 * 24 * 30) * -1)
+        memo.insertDate = randomDate
+         */
+        
+        dataList.append(["content": sentence, "insertDate": Date(timeIntervalSinceNow: Double.random(in: 0 ... 3600 * 24 * 30) * -1)])
+      }
+      
+      let insertRequest = NSBatchInsertRequest(entityName: "Memo", objects: dataList)
+      
+      // Batch insert - 여러개의 데이터를 한번에 처리
+      if let result = try mainContext.execute(insertRequest) as? NSBatchInsertResult, let succeeded = result.result as? Bool {
+        if succeeded {
+          print("Batch insert success")
+        } else {
+          print("Batch insert fail")
+        }
+      }
+      
+      // saveContext()
+    } catch {
+      print(error)
+    }
+    #endif
+  }
+  
   // 메모 목록 패칭
   func fetch() {
     let request = MemoEntity.fetchRequest()
